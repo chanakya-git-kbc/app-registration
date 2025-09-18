bd6f4234d5472449072461a0a3bdce841190c82c
cs <schanakya@hotmail.com>
2025-09-18 15:24:41 -0400

    material menu update; add mobile hamburger

--- frontend/src/app/common/menu/menu.ts
+++ frontend/src/app/common/menu/menu.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+  ],
   templateUrl: './menu.html',
   styleUrl: './menu.css'
 })
 export class Menu {
-
+  isMenuOpen = false;
 }
